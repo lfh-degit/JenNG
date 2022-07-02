b807c4efc1c9a0bb271b58167cb91cacf90596f3
afan <1436647780@qq.com>
2022-07-02 17:31:58 +0800

    test

---
+++ src/main/java/cases/TestJenkins.java
@@ -0,0 +1,10 @@
+package cases;
+
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+    @Test(groups = {"Jtest"})
+    public void Jtest(){
+    System.out.println("11111");
+    }
+}
